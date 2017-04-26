d36f700faaee1ae501d2ebc1f2258ab8713fe9a6
Marcellin Nshimiyimana <mars@fusemachines.com>
2017-04-26 11:04:22 +0545

    cleanup the code

--- src/tests/t1.js
+++ src/tests/t1.js
@@ -1,7 +1,7 @@
 var should = require('should');
 
-var q1 = require('../questions/q1.js');
-var a1 = require('../answers/a1.js');
+var q1 = require('../questions/q1.js'); // q1 = question 1
+var a1 = require('../answers/a1.js'); // a1 = answer 1
 
 // @TODO fetch user input
 var userInput = 3;
